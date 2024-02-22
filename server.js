5b4af7e5683dbd3b50c8529ca7fa95838e33ed63
Anasch113 <anaschaudry2002@gmail.com>
2024-02-22 17:07:42 +0500

    commit 2/22/24

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 require("dotenv").config()
 const express = require("express");
 const http = require("http");
-const path = require("path");
-const { AssemblyAI } = require("assemblyai");
-const WebSocket = require("ws");
+const axios = require('axios');
 const admin = require("firebase-admin");
 const bodyParser = require("body-parser");
 const cors = require("cors")
@@ -14,85 +12,40 @@ const { assert } = require("console");
 
 // ///////////////////////////////////////////////////////////////////////////////////////
 
-const aai = new AssemblyAI({ apiKey: "ce2c1d53c1af4f02a15b539ffd7bc68c" });
+
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+
 
 app.use(express.static("public"));
-app.use(
-  "/assemblyai.js",
-  express.static(
-    path.join(__dirname, "node_modules/assemblyai/dist/assemblyai.umd.js"),
-  ),
-);
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173"
+    origin: `${process.env.HOST_URL}`
   })
 )
 
-app.get("/token", async (_req, res) => {
-  const token = await aai.realtime.createTemporaryToken({ expires_in: 3600 });
-  res.json({ token });
+app.get('/token', async (req, res) => {
+  try {
+    const response = await axios.post('https://api.assemblyai.com/v2/realtime/token',
+      { expires_in: 3600 },
+      { headers: { authorization: `${process.env.AAI_KEY}` } });
+    const { data } = response;
+    res.json(data);
+  } catch (error) {
+    const { response: { status, data } } = error;
+    res.status(status).json(data);
+  }
 });
 
-
-wss.on("connection", (ws) => {
-  let rt;
-
-  ws.on("message", async (message) => {
-    const data = JSON.parse(message);
-
-    if (data.type === "start") {
-      const response = await fetch("/token");
-      const tokenData = await response.json();
-
-      if (tokenData.error) {
-        ws.send(JSON.stringify({ type: "error", error: tokenData.error }));
-        return;
-      }
-
-      rt = new AssemblyAI.RealtimeService({ token: tokenData.token });
-
-      rt.on("transcript", (transcriptMessage) => {
-        ws.send(JSON.stringify({ type: "transcript", text: transcriptMessage.text }));
-      });
-
-      rt.on("error", async (error) => {
-        console.error(error);
-        await rt.close();
-        ws.send(JSON.stringify({ type: "error", error: "RealtimeService error" }));
-      });
-
-      rt.on("close", () => {
-        rt = null;
-      });
-
-      await rt.connect();
-    } else if (data.type === "audio") {
-      if (rt) {
-        rt.sendAudio(Buffer.from(data.audio, "base64"));
-      }
-    } else if (data.type === "stop") {
-      if (rt) {
-        await rt.close(false);
-        rt = null;
-      }
-    }
-  });
-
-  ws.on("close", () => {
-    if (rt) {
-      rt.close(false);
-    }
-  });
+app.set('port', 8000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server is running on port ${server.address().port}`);
 });
-
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 
@@ -101,7 +54,7 @@ app.get("/", (req, res) => {
   res.send("Hello world")
 })
 
-const [basic, pro, business] = ["price_1Ok0PQGjRSLCL52ubBnCRhLj", "price_1Ok0R5GjRSLCL52uUfuuIwMc", "price_1Ok0RuGjRSLCL52ufBxjtq1L"]
+const [basic, pro, business] = ["price_1OknB4GjRSLCL52uZbVbapOU", "price_1Ok0R5GjRSLCL52uUfuuIwMc", "price_1Ok0RuGjRSLCL52ufBxjtq1L"]
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
@@ -120,8 +73,8 @@ const stripeSession = async (plan) => {
           quantity: 1
         }
       ],
-      success_url: "http://localhost:5173/success",
-      cancel_url: "http://localhost:5173/cancel"
+      success_url: `${process.env.HOST_URL}/success`,
+      cancel_url: `${process.env.HOST_URL}/cancel`
 
     })
     return session
@@ -136,12 +89,12 @@ app.post("/subscriptions", async (req, res) => {
   console.log(req.body);
   let planId = null;
 
-  if (plan == 29) {
+  if (plan == 10) {
     planId = basic
   }
-  else if (plan == 49) {
+  else if (plan == 20) {
     planId = pro
-  } else if (plan == 99) {
+  } else if (plan == 30) {
     planId = business
   }
 
@@ -150,14 +103,14 @@ app.post("/subscriptions", async (req, res) => {
     const user = await admin.auth().getUser(customerId);
 
     await admin.database().ref("users").child(user.uid).update({
-         subscription : {
-          sessionId: session.id
-         }
+      subscription: {
+        sessionId: session.id
+      }
 
     })
 
     console.log("session from  post request", session);
-     return res.json({session});
+    return res.json({ session });
 
   } catch (error) {
     res.send(error)
@@ -177,38 +130,39 @@ app.post("/payment-success", async (req, res) => {
     const session = await stripe.checkout.sessions.retrieve(sessionId);
 
     if (session.payment_status === 'paid') {
-        const subscriptionId = session.subscription;
-        try {
-          const subscription = await stripe.subscriptions.retrieve(subscriptionId);
-          const user = await admin.auth().getUser(firebaseId);
-          const planId = subscription.plan.id;
-          const planType = subscription.plan.amount === 2900 ? "basic" : "pro";
-          const startDate = moment.unix(subscription.current_period_start).format('YYYY-MM-DD');
-          const endDate = moment.unix(subscription.current_period_end).format('YYYY-MM-DD');
-          const durationInSeconds = subscription.current_period_end - subscription.current_period_start;
-          const durationInDays = moment.duration(durationInSeconds, 'seconds').asDays();
-          await admin.database().ref("users").child(user.uid).update({ 
-              subscription: {
-                sessionId: null,
-                planId:planId,
-                planType: planType,
-                planStartDate: startDate,
-                planEndDate: endDate,
-                planDuration: durationInDays
-              }});
-
-            
-          } catch (error) {
-            console.error('Error retrieving subscription:', error);
+      const subscriptionId = session.subscription;
+      try {
+        const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+        const user = await admin.auth().getUser(firebaseId);
+        const planId = subscription.plan.id;
+        const planType = subscription.plan.amount === 1000 ? "basic" : "pro";
+        const startDate = moment.unix(subscription.current_period_start).format('YYYY-MM-DD');
+        const endDate = moment.unix(subscription.current_period_end).format('YYYY-MM-DD');
+        const durationInSeconds = subscription.current_period_end - subscription.current_period_start;
+        const durationInDays = moment.duration(durationInSeconds, 'seconds').asDays();
+        await admin.database().ref("users").child(user.uid).update({
+          subscription: {
+            sessionId: null,
+            planId: planId,
+            planType: planType,
+            planStartDate: startDate,
+            planEndDate: endDate,
+            planDuration: durationInDays
           }
-        return res.json({ message: "Payment successful" });
-      } else {
-        return res.json({ message: "Payment failed" });
+        });
+
+
+      } catch (error) {
+        console.error('Error retrieving subscription:', error);
       }
-    } catch (error) {
-      res.send(error);
+      return res.json({ message: "Payment successful" });
+    } else {
+      return res.json({ message: "Payment failed" });
     }
-  });
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 
 
@@ -221,7 +175,3 @@ admin.initializeApp({
 
 
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});

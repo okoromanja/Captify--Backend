98918e09fed4b9ccec1a6f46f20dd2d5f4cb1b86
Anasch113 <anaschaudry2002@gmail.com>
2024-06-19 12:11:37 +0500

    email verification done

--- routes/emailSend.js
+++ routes/emailSend.js
@@ -6,6 +6,7 @@ const nodemailer = require("nodemailer")
 
 
 
+
 // Nodemailer setup 
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
@@ -21,21 +22,17 @@ const transporter = nodemailer.createTransport({
 
 
 // function to send the email verification
-const sendVerificationEmail = async (email) => {
-  const actionCodeSettings = {
-    url: `${process.env.HOST_URL}/emailverification`,
-    handleCodeInApp: true,
-  };
+const sendVerificationEmail = async (email, userId) => {
 
   try {
-    const link = await admin.auth().generateEmailVerificationLink(email, actionCodeSettings);
+    const link = `${process.env.HOST_URL}/emailverification?uid=${userId}`;
 
     const mailOptions = {
       from: process.env.NODEMAILER_USER,
       to: email,
-      subject: 'Verify your email for MyApp',
+      subject: 'Verify your email for Captify',
       text: `Please verify your email by clicking on the following link: ${link}`,
-      html: `<p>Please verify your email by clicking on the following link: <a href="${link}">Verify Email</a></p>`,
+      html: `<p>Hi, Thanks for creating the account on captify.</p> <p>Please verify your email by clicking on the following link: <a href="${link}">Verify Email</a></p>`,
     };
 
     await transporter.sendMail(mailOptions);
@@ -76,7 +73,7 @@ router.post('/signup', async (req, res) => {
     })
 
 
-    await sendVerificationEmail(userRecord.email);
+    await sendVerificationEmail(userRecord.email, userRecord.uid);
 
 
 
@@ -90,14 +87,56 @@ router.post('/signup', async (req, res) => {
 
 
 
+// Api to verify the email manually
+
+
+
+router.post('/verify-email', async (req, res) => {
+  const { uid } = req.body;
+
+  try {
+    await admin.auth().updateUser(uid, { emailVerified: true });
+    const customToken = await admin.auth().createCustomToken(uid)
+
+    res.status(200).send({ message: 'Email verified successfully', token: customToken });
+  } catch (error) {
+    console.error('Error verifying email:', error);
+    res.status(400).send({ error: error.message });
+  }
+});
 
 
 
 
 
+// Api to send that email through sendVerificationEmail() fucntion
 
 
+router.post('/email-verify-login', async (req, res) => {
+  const { email }  = req.body;
 
+  console.log("req body", req.body)
+  console.log("email", email)
+
+  try {
+
+    const userRecord = await admin.auth().getUserByEmail(email);
+
+    if (userRecord.emailVerified === false) {
+
+
+      await sendVerificationEmail(userRecord.email, userRecord.uid);
+      return res.status(200).send({ message: 'Email not verified. Verification email sent' });
+    }
+
+    return res.status(200).send({ message: 'Email verified' });
+
+
+  } catch (error) {
+    console.log('Error while login verify email', error)
+    res.status(400).send({ error });
+  }
+});
 
 
 
